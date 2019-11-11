cc056de05a32c5b52126c26447ef40798956c399
Yaozhw99 <yaozhw99@gmail.com>
2019-11-11 15:16:18 +0800

    初始化项目

---
+++ Sale/src/views/main/main.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      {{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "main",
+        data() {
+            return {
+                msg: "vue template"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
