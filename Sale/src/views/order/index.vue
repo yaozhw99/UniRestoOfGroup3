2e218034c2e0e82413e624ba861581433e32cba9
Yaozhw99 <yaozhw99@gmail.com>
2019-11-11 15:18:42 +0800

    初始化项目

---
+++ Sale/src/views/order/index.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data() {
+            return {
+                msg: "vue template"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
