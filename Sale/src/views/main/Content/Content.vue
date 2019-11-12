ed0ec414fc0839b950a82378fe4fac73000838c1
zouh13 <1650139439@qq.com>
2019-11-12 13:13:01 +0800

    11.12 13.00

--- Sale/src/views/main/Content/Content.vue
+++ Sale/src/views/main/Content/Content.vue
@@ -1,8 +1,20 @@
-<template>
-
-       
-    <div> {{msg}}</div>
-
+<template>   
+    <div>
+      <div class="shift">
+        <template>
+          <el-carousel :interval="5000" arrow="always">
+            <el-carousel-item v-for="item in shift" :key="item.id">
+              <img :src="item.src">
+            </el-carousel-item>
+          </el-carousel>
+        </template>
+      </div>
+      <el-row :gutter="20">
+        <el-col :span="3" v-for="(item,idx) in menu" :key="idx" :class="item.property?'complex':'normal'">{{item.name}}
+          <el-col :span="3" v-show="item.property" v-for="p in item.subMenu" class="complex">{{p.name}}</el-col>
+        </el-col>
+      </el-row>
+    </div>
 </template>
 
 
@@ -12,7 +24,24 @@
 
         data() {
 
-            return {msg: 'vue模板页'}
+            return {
+                shift: [
+                    {id:0,
+                    src:require("../../../icons/MainPageImg/shift.jpg")},
+                    {id:1,
+                        src:require("../../../icons/MainPageImg/shift1.jpg")},
+                    {id:2,
+                        src:require("../../../icons/MainPageImg/shift2.jpg")}
+                ],
+                menu:[{
+                    name:"特色号码"
+                      },
+                    {
+                        name:'火爆卡品',
+                        property:1,
+                        subMenu:[{name:"腾讯王卡"},{name:"冰淇淋卡"},{name:"流量王卡"}]
+                    },{}]
+            }
 
         }
 
@@ -22,6 +51,7 @@
 
 
 <style>
-
-
+.el-carousel img{
+  width: 100%;
+}
 </style>
--- Sale/src/views/main/Header/Header.vue
+++ Sale/src/views/main/Header/Header.vue
@@ -1,6 +1,17 @@
 <template> 
     <el-header>
-      {{msg}}
+     <div>
+       <div class="logo">
+         <img src="@/icons/MainPageImg/logo.png"/>
+       </div>
+       <div class="search">
+         <el-input
+           placeholder="请输入内容"
+           prefix-icon="el-icon-search">
+         </el-input>
+         <span><el-button size="small">搜索</el-button></span>
+       </div>
+     </div>
     </el-header>
 </template>
 
@@ -20,7 +31,37 @@
 </script>
 
 
-<style>
+<style scope>
+.el-header{
+  margin: 20px 60px;
+}
+.el-header div{
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+}
 
+.el-icon-search:before{
+  position: absolute;
+  margin-top: 10px;
+}
 
+.el-input__inner{
+  margin-top: 10px;
+  padding:0 !important;
+  width: 155px;
+}
+.search .el-button{
+  margin-top: 12px;
+  margin-left: 3px;
+  color: #AAAA;
+  border-radius: 5px;
+}
+
+.search .el-button:hover{
+  cursor: pointer;
+  color: cornsilk;
+  background-color: #FF6600;
+}
+input::-webkit-input-placeholder{text-align: center;}
 </style>
