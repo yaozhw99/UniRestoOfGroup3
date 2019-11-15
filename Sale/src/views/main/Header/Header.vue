1871b2de4f6c100fc647f4af8a94a1e2836b5cf8
Yaozhw99 <yaozhw99@gmail.com>
2019-11-15 17:53:51 +0800

    11.15前端页面增加模块

--- Sale/src/views/main/Header/Header.vue
+++ Sale/src/views/main/Header/Header.vue
@@ -1,18 +1,19 @@
 <template> 
-    <el-header>
-     <div>
+     <div id="app">
        <div class="logo">
          <img src="@/icons/MainPageImg/logo.png"/>
        </div>
-       <div class="search">
-         <el-input
-           placeholder="请输入内容"
-           prefix-icon="el-icon-search">
-         </el-input>
-         <span><el-button size="small">搜索</el-button></span>
+       <div class="body">
+         <div class="search">
+           <el-input
+             placeholder="请输入内容"
+             prefix-icon="el-icon-search">
+           </el-input>
+           <span><el-button size="small">搜索</el-button></span>
+         </div>
+         <div class="linkback"><a @click="goback">登录后台</a></div>
        </div>
      </div>
-    </el-header>
 </template>
 
 
@@ -24,6 +25,11 @@
 
             return {msg: '头部'}
 
+        },
+        methods:{
+            goback(){
+                this.$router.push({path:'/'})
+            }
         }
 
     }
@@ -32,14 +38,29 @@
 
 
 <style scope>
-.el-header{
-  width: 1200px;
-  margin: 20px auto;
-}
-.el-header div{
+  #app{
+    width: 1200px;
+    margin: 0 auto;
+    padding: 15px 0;
+    display: flex;
+    flex-flow: row;
+    justify-content: space-between;
+    position: relative;
+  }
+.search{
   display: flex;
-  flex-flow: row;
-  justify-content: space-between;
+}
+.body{
+
+  height: 100%;
+}
+.linkback{
+  text-align: right;
+  color: #7f7f7f;
+  font-size: 14px;
+  position: absolute;
+  right: 0;
+  bottom: 5px;
 }
 
 .el-icon-search:before{
@@ -64,5 +85,6 @@
   color: cornsilk;
   background-color: #FF6600;
 }
+
 input::-webkit-input-placeholder{text-align: center;}
 </style>
