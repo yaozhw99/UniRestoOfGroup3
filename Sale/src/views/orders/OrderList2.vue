5acca1289c9a9b4f9bf95225404b9021e9748a1f
Yaozhw99 <yaozhw99@gmail.com>
2019-11-19 20:14:46 +0800

    11.19修改数据库配置

--- Sale/src/views/orders/OrderList2.vue
+++ Sale/src/views/orders/OrderList2.vue
@@ -5,22 +5,28 @@
     <el-table-column
       prop="id"
       label="ID"
-      width="120">
+      width="120"
+      sortable>
     </el-table-column>
     <el-table-column
       prop="serialNumber"
       label="号码"
-      width="120">
+      width="120"
+      sortable>
     </el-table-column>
     <el-table-column
       prop="stateName"
       label="订单状态"
-      width="100">
+      width="120"
+      sortable
+      :filters="[{text: '已提交', value: '已提交'}, {text: '已开户', value: '已开户'}, {text: '物流派送', value: '物流派送'}, {text: '已签收', value: '已签收'}, {text: '已激活', value: '已激活'}]"
+      :filter-method="filterHandler">
     </el-table-column>
     <el-table-column
       prop="userName"
       label="用户姓名"
-      width="100">
+      width="100"
+      sortable>
     </el-table-column>
     <el-table-column
       prop="postAddress"
@@ -40,12 +46,14 @@
     <el-table-column
       prop="orderTime"
       label="订单日期"
-      width="120">
+      width="120"
+      sortable>
     </el-table-column>
     <el-table-column>
       <template slot-scope="scope">
         <el-button
           size="mini"
+          :type="bclass[scope.row.state]"
           @click="handleEdit(scope.$index, scope.row)">{{state[scope.row.state]}}</el-button>
       </template>
     </el-table-column>
@@ -61,6 +69,7 @@
             return {
                 //state:{0：订单提交，1:"已开户",2:"已发货",3:"已签收",4:"已激活"},
                 state:{0:"开户",1:"发货",2:"签收",3:"激活",4:"完成"},
+                bclass:{0:"primary",1:"success",2:"warning",3:"danger",4:"info"},
                 msg: "vue template",
                 tableData: []
             }
@@ -90,7 +99,11 @@
                         })
                 })
             }
-        }
+        },
+            filterHandler(value, row, column) {
+                const property = column['property'];
+                return row[property] === value;
+            }
     }
     }
 </script>
